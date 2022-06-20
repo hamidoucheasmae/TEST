8b54a60cbf5e56eae2906afc38621c516ddd1300
Asmae Hamidouche <76485278+hamidoucheasmae@users.noreply.github.com>
2022-06-20 18:54:48 +0100

    add

---
+++ info-tangier/src/app/components/comments/comment-form/comment-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UserInfo } from 'os';
+
+
+
+
+@Component({
+  selector: 'app-comment-form',
+  templateUrl: './comment-form.component.html',
+  styleUrls: ['./comment-form.component.scss'],
+})
+export class CommentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+
+
+}
